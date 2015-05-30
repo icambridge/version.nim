6d089931c66e827d80d10ee85786d8829bcf7b8a
Iain Cambridge <i.cambridge@foodpanda.com>
2015-05-30 08:59:41 +0200

    Reformat compare module

--- src/compare.nim
+++ src/compare.nim
@@ -9,34 +9,31 @@ proc compareNumbers(vOne: int, vTwo: int): int =
   return -1
 
 proc prepareNumber(number: string): int =
-  let chars = {'B','b','r','R'}
-  let found = number.find(chars)
+  let
+    chars = {'B','b','r','R'}
+    found = number.find(chars)
   if found == -1:
     return number.parseInt
-
-  let numberPattern = re("\\d+")
-  let foundNumber = number.findAll(numberPattern)
+  let
+    numberPattern = re("\\d+")
+    foundNumber = number.findAll(numberPattern)
   var returnVal = 0
-
   if number[0].toLower == 'b':
     returnVal = -99999
   elif number[0].toLower == 'r':
     returnVal = -88888
-
   if len(foundNumber) != 0:
     let vNum = foundNumber[0].parseInt
     returnVal += vNum
-
   return returnVal
 
 proc compare*(vOne: string, vTwo: string): int =
-
-  let vOneParts = vOne.split({'.','-'})
-  let vTwoParts = vTwo.split({'.','-'})
-  let oneNumOfParts = len(vOneParts)
-  let twoNumOfParts = len(vTwoParts)
-  let max = oneNumOfParts-1
-
+  let
+    vOneParts = vOne.split({'.','-'})
+    vTwoParts = vTwo.split({'.','-'})
+    oneNumOfParts = len(vOneParts)
+    twoNumOfParts = len(vTwoParts)
+    max = oneNumOfParts-1
   for i in countup(0, max):
     let numOne = prepareNumber(vOneParts[i])
     if i >= twoNumOfParts:
@@ -45,13 +42,11 @@ proc compare*(vOne: string, vTwo: string): int =
       elif numOne > 0:
         return 1
       return 0
-
-    let numTwo = prepareNumber(vTwoParts[i])
-    let compared = compareNumbers(numOne, numTwo)
-
+    let
+      numTwo = prepareNumber(vTwoParts[i])
+      compared = compareNumbers(numOne, numTwo)
     if compared != 0:
       return compared
-
   if twoNumOfParts > oneNumOfParts:
     let num = prepareNumber(vTwoParts[twoNumOfParts-1])
     if num < 0:
@@ -59,5 +54,4 @@ proc compare*(vOne: string, vTwo: string): int =
     elif num > 0:
       return -1
     return 0
-
   return 0
